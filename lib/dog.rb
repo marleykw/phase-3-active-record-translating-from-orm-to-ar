4a1ad09762a0bd60866344f606db5f8f4b6e6871
Marley Watson <marleykw17@gmail.com>
2023-03-21 14:43:41 -0400

    commit tree de048415c1b814e4379a804ea7851c59d1404799

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,18 @@
-class Dog
+ActiveRecord::Base.establish_connection(
+adapter: "sqlite3",
+database: "db/test.sqlite"
+)
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
+
+class Dog < ActiveRecord::Base
+
+
 end
